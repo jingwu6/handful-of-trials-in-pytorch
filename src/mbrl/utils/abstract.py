fa912e6238ec77b09cf83892d1dad8bb88931fd0
jingwu6 <37253624+jingwu6@users.noreply.github.com>
2020-11-02 11:05:05 -0600

    Delete abstract.py

--- src/mbrl/utils/abstract.py
+++
@@ -1,7 +0,0 @@
-import abc as _abc
-from abc import abstractmethod
-import overrides as _overrides
-from overrides import final, overrides
-
-class BaseClass(_abc.ABC, _overrides.EnforceOverrides):
-    pass
